21cedc7e87e334c9f1513b591ff25afb57893eb3
Jeremiah Apple <jeremiahlong777@yahoo.com.sg>
2020-06-10 15:24:29 +0800

    Done Main Activity

--- app/src/main/java/sg/edu/np/week_6_whackamole_3_0/MainActivity.java
+++ app/src/main/java/sg/edu/np/week_6_whackamole_3_0/MainActivity.java
@@ -2,6 +2,7 @@ package sg.edu.np.week_6_whackamole_3_0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     private static final String FILENAME = "MainActivity.java";
     private static final String TAG = "Whack-A-Mole3.0!";
 
+    MyDBHandler dbHandler = new MyDBHandler(this,null,null,1);
+    private TextView user;
+    private Button loginbtn;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,8 +44,44 @@ public class MainActivity extends AppCompatActivity {
             Log.v(TAG, FILENAME + ": Invalid user!");
 
         */
+        //declaring "items"
+        loginbtn = (Button)findViewById(R.id.loginBtn);
+        user = (TextView)findViewById(R.id.gotoSignUp);
 
+        //ontouch listener to go to signup
+        user.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //intent for signup page
+                Intent gotoSignUp = new Intent(MainActivity.this,Main2Activity.class);
+                gotoSignUp.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                Log.v(TAG, FILENAME + ": Create new user!");
+                startActivity(gotoSignUp);
+                return false;
+            }
+        });
 
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etUsername = findViewById(R.id.inputUser);
+                EditText etPassword = findViewById(R.id.inputPass);
+                Log.v(TAG, FILENAME + ": Logging in with: " + etUsername.getText().toString()
+                        + ": " + etPassword.getText().toString());
+
+                if (isValidUser(etUsername.getText().toString(), etPassword.getText().toString())) {
+                    Log.v(TAG, FILENAME + ": Valid User! Logging in");
+                    Intent loggedIn = new Intent(MainActivity.this, Main3Activity.class);
+                    loggedIn.putExtra("Username", etUsername.getText().toString());
+                    startActivity(loggedIn);
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.v(TAG, FILENAME + ": Invalid user!");
+                    Toast.makeText(MainActivity.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
     protected void onStop(){
@@ -51,10 +93,17 @@ public class MainActivity extends AppCompatActivity {
 
         /* HINT:
             This method is called to access the database and return a true if user is valid and false if not.
-            Log.v(TAG, FILENAME + ": Running Checks..." + dbData.getMyUserName() + ": " + dbData.getMyPassword() +" <--> "+ userName + " " + password);
+            Log.v(TAG, FILENAME + ": Running Checks..." + dbData.getMyUserName() + ": "
+            + dbData.getMyPassword() +" <--> "+ userName + " " + password);
             You may choose to use this or modify to suit your design.
          */
 
+        UserData data = dbHandler.findUser(userName);
+        if (data == null){
+            return false;
+        }
+        Log.v(TAG, FILENAME + ": Running Checks..." + data.getMyUserName() + ": "
+                + data.getMyPassword() +" <--> "+ userName + " " + password);
+        return data.getMyUserName().equals(userName) && data.getMyPassword().equals(password);
     }
-
 }
